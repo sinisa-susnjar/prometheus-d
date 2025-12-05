0d1a746ab01ec48334f3dd96ecdfbf8362449075
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2025-12-06 02:47:53 +0700

    added gc stats

--- source/prometheus/server.d
+++ source/prometheus/server.d
@@ -7,8 +7,10 @@ import std.string;
 
 import std.array;
 import std.conv;
+import core.memory;
 
 import prometheus.registry;
+import prometheus.gauge;
 
 /// --- HTTP Server ---
 void serveMetrics(Registry registry, ushort port = 8080, string host = "0.0.0.0")
@@ -19,6 +21,15 @@ void serveMetrics(Registry registry, ushort port = 8080, string host = "0.0.0.0"
   listener.listen(50);
 
   infof("prometheus metrics at http://%s:%d/metrics, registry: %s", host, port, registry);
+
+  auto gcFreeSize = registry.add(new Gauge("gc_free_size", "Free bytes on the GC heap"));
+  auto gcUsedSize = registry.add(new Gauge("gc_used_size", "Used bytes on the GC heap"));
+  auto gcMaxCollectionTime = registry.add(new Gauge("gc_max_collection_time",
+      "Largest time spent during GC cycle in µs"));
+  auto gcMaxPauseTime = registry.add(new Gauge("gc_max_pause_time", "Largest time paused during GC cycle in us"));
+  auto gcNumCollections = registry.add(new Gauge("gc_num_collections", "Total number of GC cycles"));
+  auto gcTotalCollectionTime = registry.add(new Gauge("gc_total_collection_time", "Total time spent doing GC in us"));
+  auto gcTotalPauseTime = registry.add(new Gauge("gc_total_pause_time", "Total time paused doing GC in us"));
 
   while (true) {
     try {
@@ -36,6 +47,17 @@ void serveMetrics(Registry registry, ushort port = 8080, string host = "0.0.0.0"
       string request = buffer[0 .. bytesRead].idup;
 
       if (request.startsWith("GET /metrics")) {
+
+        auto stats = GC.stats();
+        auto prof = GC.profileStats();
+        gcFreeSize.set(stats.freeSize);
+        gcUsedSize.set(stats.usedSize);
+        gcMaxCollectionTime.set(prof.maxCollectionTime.total!"usecs");
+        gcMaxPauseTime.set(prof.maxPauseTime.total!"usecs");
+        gcNumCollections.set(prof.numCollections);
+        gcTotalCollectionTime.set(prof.totalCollectionTime.total!"usecs");
+        gcTotalPauseTime.set(prof.totalPauseTime.total!"usecs");
+
         // tracef("got request from %s: %s", conn.remoteAddress(), request);
         // infof("registry: %s", registry);
         string body = registry.renderAll();
